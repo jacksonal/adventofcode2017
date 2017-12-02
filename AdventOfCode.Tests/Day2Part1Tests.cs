b7fccb369dd62d64d7afc4e10e91d4c679635849
Alex Jackson <ajackson@ebsco.com>
2017-12-02 08:07:25 -0500

    code and tests for day2 part 1 implementation

---
+++ AdventOfCode.Tests/Day2Part1Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode.Day2;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day2Part1Tests
+    {
+        private CheckSumSolver _systemUnderTest;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _systemUnderTest = new CheckSumSolver();
+        }
+
+        [Test]
+        public void GetLargestDifference_FindsLargestDiff()
+        {
+            var input = new []{12, 45, 32, 102, 76, 33};
+            var result = _systemUnderTest.GetLargestDifference(input);
+            Assert.AreEqual(90, result);
+        }
+
+        [Test]
+        public void ConvertToIntArray_ParsesStringToInt()
+        {
+            var input = "1  2   3 4";
+            var result = _systemUnderTest.ParseRow(input).ToArray();
+
+            for (int i = 1; i <= result.Length; i++)
+            {
+                Assert.AreEqual(i,result[i-1]);
+            }
+        }
+        [Test]
+        public void Solve_SumsGetLargestDiffForEachRow()
+        {
+            var input = "1  2   3 4" + Environment.NewLine + "5 6 7 8";
+            var result = _systemUnderTest.Solve(input);
+            Assert.AreEqual(6, result);
+        }
+    }
+}
---
+++ AdventOfCode/Day2/CheckSumSolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day2
+{
+    public class CheckSumSolver
+    {
+        public int GetLargestDifference(IEnumerable<int> input)
+        {
+            var sorted = input.ToList();
+            sorted.Sort();
+            return sorted.Last() - sorted.First();
+        }
+
+        public IEnumerable<int> ParseRow(string input)
+        {
+            var ret = new List<int>();
+            foreach (var val in input.Split().Where(s => s != string.Empty))
+            {
+                ret.Add(Convert.ToInt32(val));
+            }
+            return ret;
+        }
+
+        public int Solve(string input)
+        {
+            var ret = 0;
+            using (var sr = new StringReader(input))
+            {
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    var row = ParseRow(line);
+                    ret += GetLargestDifference(row);
+                    line = sr.ReadLine();
+                }
+            }
+            return ret;
+        }
+    }
+}
