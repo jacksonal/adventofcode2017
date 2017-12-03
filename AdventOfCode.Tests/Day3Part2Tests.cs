988bb09b40b4a647be83367c88d157b437cc59bd
Alex Jackson <ajackson@ebsco.com>
2017-12-03 13:18:44 -0500

    AccumulatingSpiralSolver. base test case

---
+++ AdventOfCode.Tests/Day3Part2Tests.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode.Day3;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class Day3Part2Tests
+    {
+        private AccumulatingAdjacentSpiralSolver _systemUnderTest;
+
+        [Test]
+        public void GetSquareValue_ForSquare1_Returns1()
+        {
+            _systemUnderTest = new AccumulatingAdjacentSpiralSolver();
+            Assert.AreEqual(1,_systemUnderTest.GetSquareValue(1));
+        }
+    }
+}
--- AdventOfCode/Day3/AccumulatingAdjacentSpiralSolver.cs
+++ AdventOfCode/Day3/AccumulatingAdjacentSpiralSolver.cs
@@ -1,7 +1,15 @@
 ﻿namespace AdventOfCode.Day3
 {
-    public class AccumulatingAdjacentSpiralSolver
+    public class AccumulatingAdjacentSpiralSolver : SpiralSolver
     {
-        
+        public override int Solve(int startingPoint)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetSquareValue(int squareNum)
+        {
+            return 1;
+        }
     }
 }
