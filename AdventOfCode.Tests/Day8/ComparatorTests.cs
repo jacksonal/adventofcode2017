0de01750c72306468c2a5b9894f4a3bcaea37e74
Alex Jackson <ajackson@ebsco.com>
2017-12-08 08:28:58 -0500

    creating necessary comparator classes

---
+++ AdventOfCode.Tests/Day8/ComparatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AdventOfCode.Day8;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Day8
+{
+    [TestFixture]
+    public class ComparatorTests
+    {
+        [TestCase(">",typeof(GreaterThanComparator))]
+        [TestCase("<", typeof(LessThanComparator))]
+        [TestCase(">=", typeof(GreaterThanOrEqualComparator))]
+        [TestCase("==", typeof(EqualComparator))]
+        public void GetInstance_ValidComparatorString_ReturnsCorrectInstance(string symbol, Type expected)
+        {
+            Assert.IsInstanceOf(expected, Comparator.GetInstance(symbol));
+        }
+    }
+}
---
+++ AdventOfCode/Day8/Comparator.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode.Day8
+{
+    public class Comparator
+    {
+        public static Comparator GetInstance(string symbol)
+        {
+            Comparator ret = null;
+            switch (symbol)
+            {
+                case ">":
+                    ret = new GreaterThanComparator();
+                    break;
+                case "<":
+                    ret = new LessThanComparator();
+                    break;
+                case ">=":
+                    ret = new GreaterThanOrEqualComparator();
+                    break;
+                case "<=":
+                    ret = new LessThanOrEqualComparator();
+                    break;
+                case "==":
+                    ret = new EqualComparator();
+                    break;
+            }
+            return ret;
+        }
+    }
+
+    public class EqualComparator : Comparator
+    {
+    }
+
+    public class LessThanOrEqualComparator : Comparator
+    {
+    }
+
+    public class GreaterThanOrEqualComparator : Comparator
+    {
+    }
+
+    public class LessThanComparator : Comparator
+    {
+    }
+
+    public class GreaterThanComparator : Comparator
+    {
+    }
+}
