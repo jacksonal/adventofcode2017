c883381686ccabdc28134a0e457d90b6d3abad39
Alex Jackson <alex.lee.jackson@gmail.com>
2017-12-16 10:32:37 -0500

    dance move base class and stubbed implementation

---
+++ AdventOfCode.Tests/Day16/DanceMoveTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AdventOfCode.Day16;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Day16
+{
+    public class DanceMoveTests
+    {
+        [TestCase("s1", typeof(SpinMove))]
+        [TestCase("x3/4", typeof(ExchangeMove))]
+        [TestCase("pe/b", typeof(PartnerMove))]
+        public void GetInstance_ReturnsCorrectType(string input, Type expected)
+        {
+            Assert.IsInstanceOf(expected, DanceMove.GetInstance(input));
+        }
+
+        [Test]
+        public void GetInstance_InvalidInput_ThrowsException()
+        {
+            Assert.Throws<ArgumentException>(() => DanceMove.GetInstance("bad"));
+        }
+    }
+}
---
+++ AdventOfCode/Day16/DanceMove.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AdventOfCode.Day16
+{
+    public abstract class DanceMove
+    {
+        public static DanceMove GetInstance(string move)
+        {
+            DanceMove ret;
+            var type = move[0];
+            switch (type)
+            {
+                case 'x':
+                    ret = new ExchangeMove();
+                    break;
+                case 's':
+                    ret = new SpinMove();
+                    break;
+                case 'p':
+                    ret = new PartnerMove();
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+            return ret;
+        }
+    }
+
+    public class PartnerMove : DanceMove
+    {
+    }
+
+    public class ExchangeMove : DanceMove
+    {
+    }
+
+    public class SpinMove : DanceMove
+    {
+    }
+}
